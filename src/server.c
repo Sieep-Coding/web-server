42930d54794ca199ebb904bc2e5d9608f4afd239
Alteryx-Motives <nstamb6@gmail.com>
2024-06-11 09:35:49 -0400

    bind and listen to socket before starting server

--- src/server.c
+++ src/server.c
@@ -17,5 +17,40 @@ void start_server() {
     int opt = 1;
     int addrlen = sizeof(address);
 
-    
+    if ((server_fd = socket(AF_INET, SOL_SOCKET, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    if(bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("bind failure");
+        exit(EXIT_FAILURE);
+
+    if (listen(server_fd, 3)< 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+
+    printf("Server starting on port %d\n", PORT);
+
+    while (1) {
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) {
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
+
+        handle_request(new_socket);
+
+        close(new_socket);
+    }
+    }
+    }
 }
